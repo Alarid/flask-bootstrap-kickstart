236b657b40289434c1f09e12b597de31b6d33d2e
LEGRAND Yohann <yohann.legrand@altran.com>
2019-01-24 17:28:03 +0100

    Init

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=kickstart.py
+FLASK_ENV=development
+FLASK_DEBUG=1
---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
